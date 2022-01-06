b1cca011cf2d6c8e374a1ca97e726312a9e545d4
David Rheinheimer <herr.rhein@gmail.com>
2022-01-06 16:29:26 -0600

    update default app name to SLOP

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import platform
 from tasks import app
 
-args = ['-A', 'tasks', 'worker', '-l', 'INFO', '-n', 'test']
+args = ['-A', 'tasks', 'worker', '-l', 'INFO', '-n', 'SLOP']
 if platform.system() == 'Windows':
     args.extend(['-P', 'solo'])
 app.start(args)
